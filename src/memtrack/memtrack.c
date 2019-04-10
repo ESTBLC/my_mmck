f879b79083c112f9ed0ed4669d9089891006382d
ESTBLC <estblcsk@gmail.com>
2019-04-10 01:39:42 +0100

    Add htab to store mem info

--- src/memtrack/memtrack.c
+++ src/memtrack/memtrack.c
@@ -3,16 +3,20 @@
 
 #include "memtrack.h"
 #include "strace/strace.h"
+#include "htab/htab.h"
 
 static void match_syscall(struct syscall const *syscall);
-
 static void execve_func(struct syscall const *syscall);
 static void mmap_func(struct syscall const *syscall);
 static void mprotect_func(struct syscall const *syscall);
 static void brk_func(struct syscall const *syscall);
 
+static size_t hfunc(void *);
+static bool cmpfunc(void *key1, void *key2);
+
 void memtrack(pid_t pid)
 {
+    htab_t *htab = htab_new(hfunc, cmpfunc);
     while(1)
     {
         struct syscall const *syscall = get_next_syscall(pid);
@@ -86,4 +90,24 @@ static void mprotect_func(struct syscall const *syscall)
 static void brk_func(struct syscall const *syscall)
 {
     printf("brk() = 0x%lx\n", (int64_t)syscall->return_val);
+}
+
+//Htab config
+static size_t hfunc(void *key)
+{
+    size_t hash = (size_t)key;
+    hash = (~hash) + (hash << 21); // key = (key << 21) - key - 1;
+    hash = hash    ^ (hash >> 24);
+    hash = (hash   + (hash << 3)) + (hash << 8); // key * 265
+    hash = hash    ^ (hash >> 14);
+    hash = (hash   + (hash << 2)) + (hash << 4); // key * 21
+    hash = hash    ^ (hash >> 28);
+    hash = hash    + (hash << 31);
+
+    return hash;
+}
+
+static bool cmpfunc(void *key1, void *key2)
+{
+    return key1 == key2;
 }
