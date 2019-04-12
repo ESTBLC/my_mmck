0ed5008b3a2ee098221f96c98834c2f9083938e1
ESTBLC <estblcsk@gmail.com>
2019-04-12 01:13:38 +0100

    Fix missing ;

--- src/memtrack/mem.h
+++ src/memtrack/mem.h
@@ -16,6 +16,7 @@ struct memblock *memblock_new(void *addr, size_t len, int prot);
 void memblock_insert(intrlist_t *mem_tab, struct memblock *block);
 void memblock_remove(struct memblock *block);
 
-struct memblock *memblock_find(intrlist_t *mem_tab, void *addr)
-void memblock_split(struct memblock *parent, struct memblock *child)
+struct memblock *memblock_find(intrlist_t *mem_tab, void *addr);
+void memblock_split(struct memblock *parent, struct memblock *child);
+
 #endif
