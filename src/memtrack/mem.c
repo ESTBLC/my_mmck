f96f905f728e5d0c987acbe4b4d5bfff3d08f6f6
ESTBLC <estblcsk@gmail.com>
2019-04-12 01:11:21 +0100

    Add find and split for memblock

---
+++ src/memtrack/mem.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+
+#include "mem.h"
+
+static bool memblock_contain(struct memblock *block, void *addr);
+
+struct memblock *memblock_new(void *addr, size_t len, int prot)
+{
+    struct memblock *block = malloc(sizeof(*block));
+    block->addr = addr;
+    block->len = len;
+    block->prot = prot;
+
+    return block;
+}
+
+void memblock_insert(intrlist_t *mem_tab, struct memblock *block)
+{
+    /* Sorted insert */
+    struct memblock *current;
+    intrlist_foreach(mem_tab, current, list)
+    {
+        if (block->addr > current->addr)
+        {
+            intrlist_push(&current->list, &block->list);
+        }
+    }
+}
+
+void memblock_remove(struct memblock *block)
+{
+    intrlist_remove(&block->list);
+    free(block);
+}
+
+struct memblock *memblock_find(intrlist_t *mem_tab, void *addr)
+{
+    struct memblock *block;
+    intrlist_foreach(mem_tab, block, list)
+    {
+        if (memblock_contain(block, addr))
+            return block;
+    }
+
+    return NULL;
+}
+
+void memblock_split(struct memblock *parent, struct memblock *child)
+{
+    if (!parent)
+        return;
+
+    void *end_parent = parent->addr + parent->len;
+    void *end_child = child->addr + child->len;
+
+    if (end_child < end_parent) {
+        struct memblock *tmp = memblock_new(end_child, end_parent - end_child, parent->prot);
+        memblock_insert(&parent->list, tmp);
+    }
+
+    memblock_insert(&parent->list, child);
+
+    if (parent->addr < child->addr) {
+        parent->len = child->addr - parent->addr;
+    } else {
+        memblock_remove(parent);
+    }
+}
+
+static bool memblock_contain(struct memblock *block, void *addr)
+{
+    return addr >= block->addr && addr <= block->addr + block->len;
+}
---
+++ src/memtrack/mem.h
@@ -0,0 +1,21 @@
+#ifndef MEM_H
+#define MEM_H
+
+#include <sys/types.h>
+
+#include "intrlist/intrlist.h"
+
+struct memblock {
+    void *addr;
+    size_t len;
+    int prot;
+    intrlist_t list;
+};
+
+struct memblock *memblock_new(void *addr, size_t len, int prot);
+void memblock_insert(intrlist_t *mem_tab, struct memblock *block);
+void memblock_remove(struct memblock *block);
+
+struct memblock *memblock_find(intrlist_t *mem_tab, void *addr)
+void memblock_split(struct memblock *parent, struct memblock *child)
+#endif
