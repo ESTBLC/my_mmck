58503c5f57d6f379b1b47e8a2352b301c2bd2858
ESTBLC <estblcsk@gmail.com>
2019-04-15 22:32:02 +0100

    Handle block not found for malloc & CO

--- src/memtrack/allocator.c
+++ src/memtrack/allocator.c
@@ -35,21 +35,32 @@ static void malloc_func(struct hook_info const *info, intrlist_t *mem_table)
     size_t len = info->arg1;
 
     struct memblock *block = memblock_find(mem_table, addr);
-    block = memblock_split(block, addr, len);
+    if (block != NULL) {
+        block = memblock_split(block, addr, len);
 
-    printf("malloc { addr = %p, len = %lx }\n", addr, len);
+        printf("malloc { addr = %p, len = 0x%lx }\n", addr, len);
+
+        return;
+    }
+
+    printf("!!!!!!!!Malloc no block at addr = %p for size = 0x%lx\n", addr, len);
 }
 
 static void calloc_func(struct hook_info const *info, intrlist_t *mem_table)
 {
     void *addr = (void *)info->return_val;
-    size_t nmemb = info->arg1;
-    size_t size = info->arg2;
+    size_t len = info->arg1 * info->arg2;
 
     struct memblock *block = memblock_find(mem_table, addr);
-    block = memblock_split(block, addr, nmemb * size);
+    if (block != NULL) {
+        block = memblock_split(block, addr, len);
 
-    printf("malloc { addr = %p, nmemb = %lx, size = %lx }\n", addr, nmemb, size);
+        printf("calloc { addr = %p, len = 0x%lx }\n", addr, len);
+
+        return;
+    }
+
+    printf("!!!!!!!!Calloc no block at addr = %p for size = 0x%lx\n", addr, len);
 }
 
 static void free_func(struct hook_info const *info, intrlist_t *mem_table)
@@ -57,8 +68,13 @@ static void free_func(struct hook_info const *info, intrlist_t *mem_table)
     void *addr = (void *)info->arg1;
 
     struct memblock *block = memblock_find(mem_table, addr);
+    if (block != NULL) {
+        printf("free { addr = %p, len = 0x%lx }\n", addr, block->len);
 
-    printf("free { addr = %p, len = %lx }\n", addr, block->len);
+        memblock_remove(block);
 
-    memblock_remove(block);
+        return;
+    }
+
+    printf("!!!!!!!!Free non malloced pointer!!!!!!!!\n");
 }
