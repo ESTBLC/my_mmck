859b4a33d3f1535f5c9f482b7f00abfca77b61e0
ESTBLC <estblcsk@gmail.com>
2019-04-15 22:03:31 +0100

    Malloc and free have dedicated function

--- src/memtrack/allocator.c
+++ src/memtrack/allocator.c
@@ -2,22 +2,47 @@
 #include <stdio.h>
 
 #include "allocator.h"
+#include "mem.h"
 #include "tracee/tracee.h"
 #include "preload/hook_info.h"
 #include "intrlist/intrlist.h"
+
+static void malloc_func(struct hook_info const *info, intrlist_t *mem_table);
+static void free_func(struct hook_info const *info, intrlist_t *mem_table);
 
 void match_libc(struct hook_info *info, intrlist_t *mem_table)
 {
     switch (info->type)
     {
         case MALLOC:
-            printf("This hook is a malloc\n");
+            malloc_func(info, mem_table);
             break;
         case FREE:
-            printf("This hook is a free\n");
+            free_func(info, mem_table);
             break;
         default:
             break;
     }
 }
 
+static void malloc_func(struct hook_info const *info, intrlist_t *mem_table)
+{
+    void *addr = (void *)info->return_val;
+    size_t len = info->arg1;
+
+    struct memblock *block = memblock_find(mem_table, addr);
+    block = memblock_split(block, addr, len);
+
+    printf("malloc { addr = %p, len = %lx }\n", addr, len);
+}
+
+static void free_func(struct hook_info const *info, intrlist_t *mem_table)
+{
+    void *addr = (void *)info->arg1;
+
+    struct memblock *block = memblock_find(mem_table, addr);
+
+    printf("free { addr = %p, len = %lx }\n", addr, block->len);
+
+    memblock_remove(block);
+}
